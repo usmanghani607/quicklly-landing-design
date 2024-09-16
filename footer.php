666807a4390f65e7003abaf38f37a616d258ea70
usmanghani607 <91252603+usmanghani607@users.noreply.github.com>
2024-09-16 16:46:14 +0500

    code commented

---
+++ footer.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <footer class="footer-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <div class="footer_widgth">
+                        <h4>Top Cities</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#">Chicago</a>
+                            </li>
+                            <li>
+                                <a href="#">Bay Area</a>
+                            </li>
+                            <li>
+                                <a href="#">New York</a>
+                            </li>
+                            <li>
+                                <a href="#">New Jersey</a>
+                            </li>
+                            <li>
+                                <a href="#">Austin</a>
+                            </li>
+                            <li>
+                                <a href="#">Seattle</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="footer_widgth">
+                        <h4>Our Company</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact</a>
+                            </li>
+                            <li>
+                                <a href="#">Careers</a>
+                            </li>
+                            <li>
+                                <a href="#">Faqs</a>
+                            </li>
+                            <li>
+                                <a href="#">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="#">Term & Condition</a>
+                            </li>
+                            <li>
+                                <a href="#">Seller</a>
+                            </li>
+                            <li>
+                                <a href="#">Press Release</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="footer_widgth">
+                        <h4>Find Us On</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#">facebook</a>
+                            </li>
+                            <li>
+                                <a href="#">Twitter</a>
+                            </li>
+                            <li>
+                                <a href="#">Linkedin</a>
+                            </li>
+                            <li>
+                                <a href="#">YOutube</a>
+                            </li>
+                            <li>
+                                <a href="#">Instagram</a>
+                            </li>
+                            <li>
+                                <a href="#">Pinterest</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="footer_widgth">
+                        <h4>Get In Touch With Us</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#">Phone Support: +1(224)366-0987</a>
+                            </li>
+                            <li>
+                                <a href="#">Gereral Enquiry: Hellow@Quicklly.com</a>
+                            </li>
+                            <li>
+                                <a href="#">Order Support: OrderSupport@Quicklly.com</a>
+                            </li>
+                            <li>
+                                <a href="#">Stores Support: Newstoresetup@Quicklly.com</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- <div class="col-md-3">
+                        <div class="widgth_app">
+                            <ul class="list-unstyled">
+                                <p>Download App</p>
+                                <li>
+                                    <a href=""><img src="images/ios.png" alt="playstore"></a>
+                                </li>
+                                <li>
+                                    <a href=""><img src="images/google.png" alt=""></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div> -->
+                <div class="col-md-3">
+                    <div class="widgth_app">
+                        <p>Download App</p>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <a href="#"><img src="images/ios.png" alt="ios"></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#"><img src="images/google.png" alt="playstore"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="copy-right-area">
+                    <p>
+                        Copyright©2024 Quicklly.com
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'" media="all">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="topheader">
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="navbar navbar-light">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                            <!-- <span class="navbar-toggler-icon"></span> -->
+                             <img src="images/breadcrum.png" alt="">
+                        </button>
+                        <div class="col-md-1">
+                            <div class="logo-sec">
+                                <img src="images/logo2.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="zip-code">
+                                <h5>Shopping in</h5>
+                                <span>1400 N Lake Shore</span>
+                                <span><img src="images/down.png" alt=""></span>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="search">
+                                <form class="d-flex search-input">
+                                    <input class="form-control me-2" type="search" placeholder="Search for products…" aria-label="Search">
+                                    <i class="fa-solid fa-magnifying-glass"></i>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <div class="login">
+                                <span>Login</span>
+                                <span><img src="images/down.png" alt=""></span>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <div class="return">
+                                <p>Return</p>
+                                <h5>& Orders</h5>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <div class="cart">
+                                <span><i class="fa-solid fa-cart-shopping"></i></span>
+                                <span class="cart-text">Cart</span>
+                                <span class="number">0</span>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                <div class="offcanvas offcanvas-start sidebar" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header sidebar-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Hello Sanjay</h5>
+                        <button type="button" class="btn" data-bs-dismiss="offcanvas" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
+                    </div>
+                    <div class="offcanvas-body sidebar-body">
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item heading">
+                                <a class="nav-link" aria-current="page" href="#">Shop by category</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Grocery</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Food</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Nationwide</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Direct from India</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Astrology (Shubh Puja)</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Events</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Moments</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Just By Quicklly Meals</a>
+                            </li>
+                            <div class="aa">
+
+                            </div>
+                            <li class="nav-item heading">
+                                <a class="nav-link" href="#">Programs & Features</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Q Pay</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Events</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Recipe</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Quicklly Pass</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Gift Card</a>
+                            </li>
+                            <li class="nav-item heading">
+                                <a class="nav-link" href="#">Account & Settings</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Your Account</a>
+                            </li>
+                            <li class="nav-item sub-heading">
+                                <a class="nav-link" href="#">Sign out</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
